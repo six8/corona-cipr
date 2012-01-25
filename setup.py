5368ca39d719f0b49e7b8cee00d3b8bff2afa558
Mike Thornton <msthornton@gmail.com>
2012-01-24 20:14:25 -0600

    Added URL

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ def main():
         version = open('VERSION.txt').read().strip(),
         author='Mike Thornton',
         author_email='six8@devdetails.com',
+        url='http://github.com/six8/corona-cipr',
         download_url='http://github.com/six8/corona-cipr',
         keywords=['corona'],
         license='MIT',
